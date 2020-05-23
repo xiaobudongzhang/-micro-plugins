b5c5c6348a0c62f270df155ec1b378cebaedd81a
baoyazhang <baoyazhang@dalongyun.com>
2020-05-23 22:09:13 +0800

    add

---
+++ tracer/opentracing/std2micro/ std2micro.go
@@ -0,0 +1,59 @@
+package std2micro
+
+import (
+	"context"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-plugins/wrapper/trace/opentracing"
+	"github.com/opentracing/opentracing-go"
+)
+
+var sf = 100
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func SetSamplingFrequency(n int) {
+	sf = n
+}
+
+func TracerWrapper(n http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		md := make(map[string]string)
+		spanCtx, _  := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, opentracing.HTTTPHeadersCarrier(r.Header))
+		sp := opentracing.GlobalTracer().StartSpan(r.URL.Path, opentracing.ChildOf(spanCtx))
+
+		defer sp.Finsh()
+
+		if err := opentracing.GlobalTracer().Inject {
+			sp.Context(),
+			opeopentracing.TextMap,
+			opeopentracing.TextMapCarrier(md); err!= nil {
+				log.Println(err)
+			}
+		}
+
+
+		ctx := context.TODO()
+		ctx = opentracing.ContextWithSpan(ctx, sp)
+		ctx = metadata.NewContent(ctx, md)
+		r = r.WithContext(ctx)
+
+		sct := &statusCode.StautsCodeTracker{http.ResponseWriter:w, status:http.StatusOK}
+		h.ServerHTTP(sct.WrappedReponseWriter(), r)
+		
+
+		ext.HTTPMethod.Set(sp, r.Method)
+		ext.HTTPUrl.Set(sp, r.URL.EscapedPath())
+		ext.HTTPStatusCode.Set(sp, uinit16(sct.Status))
+		if sct.Status > http.StatusInternalServerError {
+			ext.Error.Set(sp, true)
+		} else if rand.Intn(100) > sf {
+			ext.SamplingPrority.Set(sp, 0)
+		}
+	})
+}
