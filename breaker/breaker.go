ab9def9eca542798c9bf73ba771bb24b7deb5567
baoyazhang <baoyazhang@dalongyun.com>
2020-05-23 17:11:17 +0800

    降级

--- breaker/breaker.go
+++ breaker/breaker.go
@@ -14,11 +14,10 @@ func BreakerWrapper(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		name := r.Method + "-" + r.RequestURI
 		hystrix.Do(name, func() error {
-
 			sct := &statusCode.StatusCodeTracker{ResponseWriter: w, Status: http.StatusOK}
 			h.ServeHTTP(sct.WrappedResponseWriter(), r)
 
-			if sct.Status > http.StatusInternalServerError {
+			if sct.Status >= http.StatusInternalServerError {
 				str := fmt.Sprintf("status code %d", sct.Status)
 				return errors.New(str)
 			}
